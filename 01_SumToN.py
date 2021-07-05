a0370e4c18fe639e969dfd718203e851e16991a8
Esantomi <pilinnasa@gmail.com>
2021-07-05 16:11:00 +0900

    Gauss summation added

--- 01_SumToN.py
+++ 01_SumToN.py
@@ -1,5 +1,4 @@
 # 1부터 n까지의 합 구하기
-
 # input: n
 # output: 1부터 n까지의 합
 
@@ -10,3 +9,14 @@ def sum_n(n):
     return s
 
 print(sum_n(5))
+print(sum_n(1001))
+
+
+# 다른 알고리즘으로 구하기
+# Gauss Summation, n(n+1)/2
+
+def sum_gauss(n):
+    return (n*(n+1))//2  # 정수 나눗셈 (소수점 자리 없게끔)
+
+print(sum_gauss(5))
+print(sum_gauss(1001))
