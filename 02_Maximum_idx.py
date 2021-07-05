38c886b1efd85da1ee2c3dc68285df5d4a3cf927
Esantomi <pilinnasa@gmail.com>
2021-07-05 18:34:26 +0900

    codes from the book added

--- 02_Maximum_idx.py
+++ 02_Maximum_idx.py
@@ -17,3 +17,13 @@ print(max_idx(l2))
 
 
 # 책 풀이
+def find_max_idx(a):
+    n = len(a)
+    max_idx = 0
+    for i in range(1, n):
+        if a[i] > a[max_idx]:
+            max_idx = i
+    return max_idx
+
+v = [17, 92, 18, 33, 58, 7, 33, 42]
+print(find_max_idx(v))
