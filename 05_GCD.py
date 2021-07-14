523fe96caa119d471e154c8095212a2a845b960e
Esantomi <pilinnasa@gmail.com>
2021-07-14 10:52:01 +0900

    GCD algorithm

--- 05_GCD.py
+++ 05_GCD.py
@@ -8,16 +8,25 @@
 def Gcd(a, b):
     common_div = min(a, b)
     while True:
-        if a % common_div == 0 & b % common_div == 0:
+        if a % common_div == 0 and b % common_div == 0:
             return common_div
         else: common_div -= 1
 
 print(Gcd(1, 5))
 print(Gcd(3, 6))
+print(Gcd(60, 24))
+print(Gcd(81, 27))
 
 
 # 책 풀이
 def gcd(a, b):
     i = min(a, b)
     while True:
-        
+        if a % i == 0 and b % i == 0:
+            return i
+        i = i - 1
+
+print(gcd(1, 5))  # 1
+print(gcd(3, 6))  # 3
+print(gcd(60, 24))  # 12
+print(gcd(81, 27))  # 27
